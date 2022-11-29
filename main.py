342388c555d0e91b022d0b2b8b3624668326b41f
Cade Lawless <78225446+CadeLawless@users.noreply.github.com>
2022-11-29 03:01:20 +0000

    created index.html and welcome.html and added function to main.py

--- main.py
+++ main.py
@@ -1,52 +1,93 @@
-from flask import Flask, request
-from caesar import rotate_string
+from flask import Flask, render_template, request
 
 app = Flask('app')
 
-form = '''
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-            .error {{
-                color: red;
-            }}
-        </style>
-    </head>
-    <body>
-      <form action="/" method="POST">
-        <label for="rot">Rotate by:
-        <input type="text" name="rot" value="0" />
-        <p class="error"></p>
-        </label>
-        <textarea name="text">{0}</textarea>
-        <input type="submit" value="Encrypt Text" />
-      </form>
-    </body>
-</html>
-'''
-
 @app.route('/')
 def index():
-    return form.format('')
+    return render_template('index.html')
+
 @app.route('/', methods=['POST'])
-def encrypt():
-    rot = request.form['rot']
-    text = request.form['text']
-    return form.format(rotate_string(text, int(rot)))
+def checkErrors():
+  username = request.form['username']
+  email = request.form['email']
+  password = request.form['password']
+  confirmPassword = request.form['confirmPassword']
+  usernameError = ""
+  emailError = ""
+  passwordError = ""
+  confirmPasswordError = ""
+  errorCount = 0
+  if username == "":
+    usernameError = "This field needs to be filled out"
+    errorCount += 1
+  else:
+    if " " in username:
+      usernameError = "Username cannot have a space in it"
+      errorCount += 1
+    else:
+      if len(username) < 3 or len(username) > 20:
+        usernameError = "Username needs to be between 3 and 20             characters"
+        errorCount += 1
+  if email != "":
+    if " " in email:
+      emailError = "Email cannot have a space in it"
+      errorCount += 1
+    else:
+      if "@" in email and "." in email:
+        atCount = 0
+        periodCount = 0
+        for char in email:
+          if char == "@":
+            atCount += 1
+        if atCount > 1:
+          emailError = "There can only be one '@' in the email"
+          errorCount += 1
+        for char in email:
+          if char == ".":
+            periodCount += 1
+        if periodCount > 1:
+          emailError = "There can only be one '.' in the email"
+          errorCount += 1
+        if len(email) < 3 or len(email) > 20:
+          emailError = "Email needs to be between 3 and 20                   characters"
+          errorCount += 1
+      else:
+        emailError = "Email has to have '@' and '.' in it"
+        errorCount += 1
+  if password == "":
+    passwordError = "This field needs to be filled out"
+    errorCount += 1
+  else:
+    if " " in password:
+      passwordError = "Password cannot have a space in it"
+      errorCount += 1
+    else:
+      if len(password) < 3 or len(password) > 20:
+        passwordError = "Password needs to be between 3 and 20             characters"
+        errorCount += 1
+      else:
+        if password != confirmPassword:
+          passwordError = "Passwords do not match"
+          errorCount += 1
+  if confirmPassword == "":
+    confirmPasswordError = "This field needs to be filled out"
+    errorCount += 1
+  if errorCount > 0:
+    return render_template(
+      "index.html",
+      usernameError = usernameError,
+      emailError=emailError,
+      passwordError=passwordError,
+      confirmPasswordError=confirmPasswordError,
+      username=username,
+      email=email
+    )
+  else:
+    return render_template(
+      "welcome.html",
+      username=username
+    )
+        
+        
 
 app.run(host='0.0.0.0', port=8080)
